492bcaead06523d35cedc46140ffb532683d7e35
spylak <spylak@hotmail.com>
2022-12-11 23:41:15 +0200

    strapi update

--- tavern-cms/config/database.ts
+++ tavern-cms/config/database.ts
@@ -1,13 +1,33 @@
-export default ({ env }) => ({
-  connection: {
-    client: 'postgres',
+function isProd(){
+  return false;
+}
+export default ({ env }) =>{
+  if(isProd()){
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST', '172.1.1.23'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'tavern-cms-prod'),
+          user: env('DATABASE_USERNAME', 'postgres'),
+          password: env('DATABASE_PASSWORD', 'Admin123!'),
+          ssl: env.bool('DATABASE_SSL', false),
+        },
+      },
+    }
+  }
+  return{
     connection: {
-      host: env('DATABASE_HOST', '172.1.1.23'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'tavern-cms'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'Admin123!'),
-      ssl: env.bool('DATABASE_SSL', false),
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'tavern-cms'),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'Admin123!'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
     },
-  },
-});
+  }
+};
