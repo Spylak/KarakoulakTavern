73dbb1608680da24ad64fb6358d6778cefd7f201
spylak <spylak@hotmail.com>
2022-12-16 16:48:25 +0200

    Added foundations for 2 languages.  Added redirect for not found.

--- tavern-mpa/src/env.d.ts
+++ tavern-mpa/src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+    readonly API_KEY: string;
+    readonly CMS_URL: string;
+    // more env variables...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
